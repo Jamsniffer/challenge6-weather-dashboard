d6349e0d0f65901be22186fca08407c931489c2c
Royal Jam <103871471+Jamsniffer@users.noreply.github.com>
2022-06-15 21:15:29 -0700

    communication with api confirmed

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
+const long = "-122.67563"
+const lat = "45.51179"
 
+function getWeatherData() {
+    const apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat +"&lon="+ long +"&exclude={part}&appid=51ae77056fd9abd11ca5e1e8a37541e0"
+
+    fetch(apiUrl) 
+        .then(function(response){
+            console.log(response)
+            todayForecast();
+        });
+};
+
+function todayForecast(){
+
+}
+
+getWeatherData()
